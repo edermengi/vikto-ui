146cbe264d4c618d5385373ec324398b18bd6443
Yevhenii Dermenzhii <dzhonu@ukr.net>
2022-08-24 10:53:10 +0300

    Show rounds bar only if game is active

--- src/components/AppHeader/RoundBar.js
+++ src/components/AppHeader/RoundBar.js
@@ -1,18 +1,27 @@
 import {useSelector} from "react-redux";
-import {getQuestionNo, getRoundNo, getTotalNumberOfQuestions, getTotalNumberOfRounds} from "../../app/gameSlice";
+import {
+    getGameState,
+    getQuestionNo,
+    getRoundNo,
+    getTotalNumberOfQuestions,
+    getTotalNumberOfRounds
+} from "../../app/gameSlice";
 import {Chip} from "@mui/material";
 
 export default function RoundBar() {
 
+    const gameState = useSelector(getGameState);
     const questionNo = useSelector(getQuestionNo);
     const roundNo = useSelector(getRoundNo);
     const totalNumberOfRounds = useSelector(getTotalNumberOfRounds);
     const totalNumberOfQuestions = useSelector(getTotalNumberOfQuestions);
 
-    return (
-        <div>
+    if (gameState) {
+        return <div>
             <Chip label={"Раунд " + roundNo + "/" + totalNumberOfRounds}/>
             <Chip label={"Вопрос " + questionNo + "/" + totalNumberOfQuestions}/>
-        </div>
-    );
+        </div>;
+    } else {
+        return <></>;
+    }
 }
